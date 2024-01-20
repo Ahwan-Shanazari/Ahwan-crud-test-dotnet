219fa81065faf15c878310023e273175b08a9be6
Ahwan-Shanazari <ahwanshanazari2332@gmail.com>
2024-01-20 10:52:30 +0330

    CacheContext and its view model created for CQRS implementation

---
+++ Mc2.CrudTest.Core/Application/ViewModels/CustomerView.cs
@@ -0,0 +1,6 @@
+﻿using Domain.Customers;
+
+namespace Application.ViewModels;
+
+public record CustomerView(string FirstName, string LastName, DateTime DateOfBirth, PhoneNumber PhoneNumber,
+    string Email, string BankAccountNumber);
---
+++ Mc2.CrudTest.Persistence/Contexts/CacheContext.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Concurrent;
+using Application.ViewModels;
+
+namespace Mc2.CrudTest.Persistence.Contexts;
+
+public class CacheContext
+{
+    public ConcurrentDictionary<int, CustomerView> Customers { get; } = new();
+}
