8c3e91041a993531874cd2f3543545ed42e8691c
Ahwan-Shanazari <ahwanshanazari2332@gmail.com>
2024-01-20 10:29:56 +0330

    data layer renamed to Persistence to fit clean architecture also context and CustomerConfigurations added

---
+++ Mc2.CrudTest.Persistence/Configurations/CustomerConfigurations.cs
@@ -0,0 +1,19 @@
+﻿using Domain.Customers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Mc2.CrudTest.Persistence.Configurations;
+
+public class CustomerConfigurations:IEntityTypeConfiguration<Customer>
+{
+    private const int PhoneNumberMaxlength = 50;
+    private const int CountryCodeMaxlength = 10;
+    public void Configure(EntityTypeBuilder<Customer> builder)
+    {
+        builder.OwnsOne(customer => customer.PhoneNumber, numberBuilder =>
+        {
+            numberBuilder.Property(number => number.Number).HasMaxLength(PhoneNumberMaxlength);
+            numberBuilder.Property(number => number.CountryCode).HasMaxLength(CountryCodeMaxlength);
+        });
+    }
+}
---
+++ Mc2.CrudTest.Persistence/Contexts/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Customers;
+using Mc2.CrudTest.Persistence.Configurations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mc2.CrudTest.Persistence.Contexts;
+
+public class ApplicationContext : DbContext
+{
+    public DbSet<Customer> Customers { get; set; }
+    
+    public ApplicationContext(DbContextOptions<ApplicationContext> options)
+        : base(options)
+    {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.ApplyConfiguration(new CustomerConfigurations());
+    }
+}
